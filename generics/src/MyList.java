b03a19bca581b5229191f2a541af7f7b60b1a942
Onur Nasır <onur.nasr@gmail.com>
2020-08-03 00:36:53 +0300

    Generics Example Added.

---
+++ generics/src/MyList.java
@@ -0,0 +1,9 @@
+public class MyList<T> { //T yazmamızın nedeni Type yani tipten gelir.Bu bir isimlendirme standardıdır.
+    public void add(T value){
+
+    }
+    public void remove(T value){
+
+    }
+
+}
